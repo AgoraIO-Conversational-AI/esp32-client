e8c8c029a209ab59c3400dd6f6ebf051dca26e85
lily <lily>
2026-03-25 18:41:31 +0800

    update app_config.h

---
+++ esp32-korvo-v3/main/app_config.h
@@ -0,0 +1,52 @@
+{
+    "name": "esp32_conversation",
+    "properties": {
+        "channel": "esp32_conversation",
+        "agent_rtc_uid": "1234",
+        "remote_rtc_uids": [
+            "5678"
+        ],
+        "enable_string_uid": false,
+        "idle_timeout": 120,
+        "advanced_features": {
+          "enable_aivad": false,
+          "enable_bhvs": true,
+          "enable_tools": true
+        },
+        "parameters": {
+          "output_audio_codec": "PCMU",
+          "agent_rtc_sdk_params": "{\"che.audio.acm_ptime\": 20}",
+          "enable_dump": true,
+          "transcript": {
+            "enable": false,
+            "redundant": false,
+            "enable_words": false
+          },
+          "data_channel": "datastream"
+        },
+        "llm": {
+            "url": "YOUR LLM URL",
+            "api_key": "YOUR  LLM API KEY",
+            "input_modalities":["text", "image"],
+            "predefined_tools": ["_publish_message"],
+            "system_messages": [
+                {
+                    "role": "system",
+                    "content": "you are a helpful robot!"
+                }
+            ],
+            "greeting_message": "Hello, how can I assist you today?",
+            "failure_message": "Please hold on a second.",
+            "params": {
+                "model": "gpt-4o"
+            }
+        },
+        "tts": {
+            "vendor": "YOUR TTS VENDOR",
+            "params": {
+                "key": "YOUR TTS KEY",
+                "voice_name": "TTS VOICE NAME"
+            }
+        }
+    }
+}
--- esp32-korvo-v3/main/project_config.h
+++ esp32-korvo-v3/main/project_config.h
@@ -1,11 +1,11 @@
 #pragma once
 
 #define AGORA_AI_AGENT_API_URL "https://api.agora.io/api/conversational-ai-agent/v2/projects"
-#define AGORA_API_KEY          "a4d6715d65d74a2a9597a2290cb4db6f"
-#define AGORA_API_SECRET       "888cc980cfd94261af11140cd2f03b0f"
+#define AGORA_API_KEY          "YOUR AGORA_API_KEY"
+#define AGORA_API_SECRET       "YOUR AGORA_API_SECRET"
 
-#define AGORA_APP_ID           "7aaa1669f4e64a49887b40fdcd24754c"
-#define AGORA_RTC_TOKEN        ""
+#define AGORA_APP_ID           "YOUR AGORA_APP_ID"
+#define AGORA_RTC_TOKEN        ""        //optional
 
-#define WIFI_SSID              "agora.guest"
-#define WIFI_PASSWORD          "8estv0ip2022!"
+#define WIFI_SSID              "YOUR WIFI_SSID"
+#define WIFI_PASSWORD          "YOUR WIFI_PASSWORD"
